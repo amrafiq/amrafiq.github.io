dc32ac73ef1c7bd0e046c604a1130fdefc77674c
alidoe <Ali@Munnas-MacBook-Pro.local>
2017-07-07 08:52:38 -0700

    contact

---
+++ postEmail.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Message Sent</title>
+</head>
+<body>
+<?php
+    $myEmailAddress = "amrafiq@asu.edu";
+
+    $subject = $_POST['Subject'];
+    $name = $_POST['Name'];
+    $email = $_POST['Email'];
+    $message = $_POST['Message'];
+    $header = "from: $name <$email>";
+    
+    mail($myEmailAddress, $name, $subject, $message, $header);
+?>
+    
+    <p>Thanks!</p>
+</body>
+</html>
